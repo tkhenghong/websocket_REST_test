3a47ce47d9f91126b7fee455cb8f44c2e485e33a
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-12-02 01:00:23 +0800

    Fixed Failed to retrospect class AbstractSecurityWebSocketMessageBrokerConfigurer when Annotate WebSocketSecurityConfig class as @Configuration
    Unable to start up Spring Boot project during merging Spring Boot Security with Spring Boot WebSocket Security

--- src/main/java/com/example/websocketdemo/config/SecurityConfiguration.java
+++ src/main/java/com/example/websocketdemo/config/SecurityConfiguration.java
@@ -1,10 +1,7 @@
 package com.example.websocketdemo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,19 +11,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @EnableWebSecurity
-//@ComponentScan("com.example.websocketdemo")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//    @Autowired
-//    SecurityConfiguration() {
-//    }
+    @Autowired
+    SecurityConfiguration() {
+    }
 
     // Main thing. WL he just used XML format to write all these things
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // permitAll() to allow all in authorizations.
         http.csrf()
-//                .and()
-//                .headers()
-//                .frameOptions().sameOrigin()
                 .disable()
                 .authorizeRequests()
                 .antMatchers(
@@ -37,6 +31,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // Enable HTTPS
-//        http.requiresChannel().anyRequest().requiresSecure();
+        // http.requiresChannel().anyRequest().requiresSecure();
     }
 }
--- src/main/java/com/example/websocketdemo/config/WebSocketSecurityConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketSecurityConfig.java
@@ -8,21 +8,17 @@ import static org.springframework.messaging.simp.SimpMessageType.*;
 
 // This is used to filter which type of SimpMessageType is allowed to connect with WebSocket. This is an authorization filter.
 // https://docs.spring.io/spring-security/site/docs/4.2.x/reference/html/websocket.html#websocket-authorization
-// TODO: Unable to run application with WebSocketSecurityConfig***
 @Configuration
 public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
     @Override
     protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
         // This is within Spring Boot Security for WebSocket.
-//        messages
-//                .nullDestMatcher().authenticated()
-//                .simpTypeMatchers(CONNECT).permitAll()
-//                .simpTypeMatchers(SUBSCRIBE, MESSAGE, UNSUBSCRIBE, DISCONNECT).authenticated()
-//                .simpDestMatchers("/secured/**").authenticated()
-//                .anyMessage().denyAll();
         messages
-                .simpDestMatchers("/secured/**").permitAll()
-                .anyMessage().permitAll();
+                .nullDestMatcher().authenticated()
+                .simpTypeMatchers(CONNECT).permitAll()
+                .simpTypeMatchers(SUBSCRIBE, MESSAGE, UNSUBSCRIBE, DISCONNECT).authenticated()
+                .simpDestMatchers("/secured/**").authenticated()
+                .anyMessage().denyAll();
     }
 
     @Override
